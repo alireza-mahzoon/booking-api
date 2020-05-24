31b4ba7c628c415f27c9ecfa7e581c853008b214
alirezamah <alireza.mahzoon44@gmail.com>
2020-05-24 16:05:15 +0200

    Fix directory structure

--- booking-api/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Hotel (
-    Id BIGSERIAL PRIMARY KEY,
-    Name TEXT NOT NULL,
-    Address TEXT NOT NULL,
-    City TEXT NOT NULL,
-    Country TEXT NOT NULL,
-    Registered TIMESTAMP NOT NULL
-);
