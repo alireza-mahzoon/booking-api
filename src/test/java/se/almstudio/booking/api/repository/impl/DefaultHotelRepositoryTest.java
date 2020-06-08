8e3ce3fed45542552f9842766141f5086837e11b
Alireza Mahzoon <alireza.mahzoon44@gmail.com>
2020-06-08 21:15:46 +0200

    add unit test for hotel creation

---
+++ src/test/java/se/almstudio/booking/api/repository/impl/DefaultHotelRepositoryTest.java
@@ -0,0 +1,23 @@
+package se.almstudio.booking.api.repository.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.almstudio.booking.api.model.entity.Hotel;
+
+import java.time.LocalDateTime;
+
+public class DefaultHotelRepositoryTest {
+    @Test
+    public void testCreateHotelExpectNoneNullId() {
+        DefaultHotelRepository hotelRepository = new DefaultHotelRepository();
+        Hotel hotel = new Hotel();
+        hotel.setName("Scandic");
+        hotel.setCity("Stockholm");
+        hotel.setCountry("Sweden");
+        hotel.setAddress("Sundby");
+        hotel.setRegistered(LocalDateTime.now());
+        Long result = hotelRepository.create(hotel);
+        Assert.assertNotNull(result);
+        Assert.assertNotEquals(0L, result.longValue());
+    }
+}
