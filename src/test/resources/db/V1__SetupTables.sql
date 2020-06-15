c10f0ad5f2211dd7cd65472e279421b411519521
Alireza Mahzoon <alireza.mahzoon44@gmail.com>
2020-06-15 07:09:45 +0200

    test ci badge

--- src/test/resources/db/V1__SetupTables.sql
+++ src/test/resources/db/V1__SetupTables.sql
@@ -1,6 +1,6 @@
 create table Hotel (
     Id bigserial primary key,
-    Name TEXT NOT NULL,
+    Name TEXT not null,
     Address text not null,
     City text not null,
     Country text not null,
