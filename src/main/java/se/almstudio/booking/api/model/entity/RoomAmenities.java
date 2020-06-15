aef4d78da8f49271b6f972744d388998f0c83039
Alireza Mahzoon <alireza.mahzoon44@gmail.com>
2020-06-15 22:43:23 +0200

    create a table to record user data

--- src/main/java/se/almstudio/booking/api/model/entity/RoomAmenities.java
+++
@@ -1,6 +0,0 @@
-package se.almstudio.booking.api.model.entity;
-
-public class RoomAmenities {
-
-
-}
---
+++ src/main/java/se/almstudio/booking/api/model/entity/User.java
@@ -0,0 +1,69 @@
+package se.almstudio.booking.api.model.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String birthday;
+  private String email;
+  private LocalDateTime Registered;
+  private LocalDateTime updated;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setBirthday(String birthday) {
+    this.birthday = birthday;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setRegistered(LocalDateTime registered) {
+    Registered = registered;
+  }
+
+  public void setUpdated(LocalDateTime updated) {
+    this.updated = updated;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public LocalDateTime getRegistered() {
+    return Registered;
+  }
+
+  public LocalDateTime getUpdated() {
+    return updated;
+  }
+}
