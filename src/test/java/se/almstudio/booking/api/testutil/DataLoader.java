48a9767badd4baa34a98e8c0d414d755991a51f3
Alireza Mahzoon <alireza.mahzoon44@gmail.com>
2020-06-19 15:00:31 +0200

    edit dataloader to geberate data

--- src/test/java/se/almstudio/booking/api/testutil/DataLoader.java
+++ src/test/java/se/almstudio/booking/api/testutil/DataLoader.java
@@ -2,11 +2,13 @@ package se.almstudio.booking.api.testutil;
 
 import com.github.javafaker.Faker;
 import org.junit.Test;
-import se.almstudio.booking.api.model.entity.Hotel;
-import se.almstudio.booking.api.model.entity.HotelAmenity;
-import se.almstudio.booking.api.model.entity.RoomAmenity;
+import se.almstudio.booking.api.model.entity.*;
 import se.almstudio.booking.api.repository.*;
 import se.almstudio.booking.api.repository.impl.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataLoader {
   private final HotelRepository hotelRepository = new DefaultHotelRepository();
@@ -20,18 +22,43 @@ public class DataLoader {
 
   @Test
   public void testGenerateData() {
-    generate(10,0,0,0,0, 0, 0);
+    generate(10,10,10,0,0, 0, 0);
   }
 
   public void generate(int hotels, int roomTypes, int rooms, int roomAmenities, int hotelAmenities, int users, int bookings) {
 
+    List<Long> hotelId = new ArrayList<>();
+    List<Long> roomTypeId = new ArrayList<>();
+
+
     for (int i = 0; i < hotels; i++) {
-      Hotel hotel = new Hotel();
-      hotel.setName(faker.name().name());
-      hotel.setAddress(faker.address().fullAddress());
-      hotel.setCity(faker.address().city());
-      hotel.setCountry(faker.address().country());
-      hotelRepository.create(hotel);
+        Hotel hotel = new Hotel();
+        hotel.setName(faker.name().name());
+        hotel.setAddress(faker.address().fullAddress());
+        hotel.setCity(faker.address().city());
+        hotel.setCountry(faker.address().country());
+        Long result = hotelRepository.create(hotel);
+        hotelId.add(result);
+    }
+
+    for (int i = 0; i < roomTypes; i++) {
+      RoomType roomType = new RoomType();
+      roomType.setHotelId(hotelId.get(i));
+      roomType.setName(faker.name().name());
+      roomType.setDescription(faker.lorem().sentence());
+      roomType.setCapacity(faker.number().numberBetween(2,6));
+      Long result = roomTypeRepository.create(roomType);
+      roomTypeId.add(result);
+    }
+
+    for (int i = 0; i < rooms; i++) {
+      Room room = new Room();
+      room.setHotelId(hotelId.get(i));
+      room.setRoomTypeId(roomTypeId.get(i));
+      room.setNumber((int) faker.number().randomNumber());
+      room.setPhoneNumber(faker.phoneNumber().toString());
+      room.setFloor(faker.number().numberBetween(1,200));
+      roomRepository.create(room);
     }
   }
 }
