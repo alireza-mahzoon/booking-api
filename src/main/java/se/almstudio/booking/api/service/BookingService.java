58245112b7d6eab92b28a5946e1c1867781da5c5
Alireza Mahzoon <alireza.mahzoon44@gmail.com>
2020-06-26 20:58:27 +0200

    add a method to find a hotel with selected foilters

--- src/main/java/se/almstudio/booking/api/service/BookingService.java
+++ src/main/java/se/almstudio/booking/api/service/BookingService.java
@@ -1,6 +1,7 @@
 package se.almstudio.booking.api.service;
 
 import se.almstudio.booking.api.model.entity.Booking;
+import se.almstudio.booking.api.model.entity.CityCountryRoomType;
 import se.almstudio.booking.api.model.entity.Hotel;
 import se.almstudio.booking.api.model.entity.RoomType;
 
@@ -10,28 +11,12 @@ import java.util.List;
 public interface BookingService {
 
   /**
-   * Find available hotelIds between checkInDate and checkOutDate
-   *
-   * @param checkindate CheckIn Date
-   * @param checkoutdate CheckOut Date
-   * @return Booking if found, {@code null} if not found
-   */
-  List<Booking> findBooking(LocalDate checkindate, LocalDate checkoutdate);
-
-  /**
-   * Find hotels in the city and country
+   * Find list of hotels with a specific roomtype in a city and country
    *
    * @param city City
    * @param country Country
+   * @param roomTypeName RoomTypeName
    * @return Hotel if found, {@code null} if not found
    */
-  List<Hotel> findHotelByCityAndCountry(String city, String country);
-
-  /**
-   * Find roomtypes with the Capacity
-   *
-   * @param capacity Capacity
-   * @return Hotel if found, {@code null} if not found
-   */
-  List<RoomType> findRoomType(int capacity);
+  List<CityCountryRoomType> findHotelByCityAndCountry(String city, String country, String roomTypeName);
 }
--- src/main/java/se/almstudio/booking/api/service/impl/DefaultBookingService.java
+++ src/main/java/se/almstudio/booking/api/service/impl/DefaultBookingService.java
@@ -2,9 +2,8 @@ package se.almstudio.booking.api.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import se.almstudio.booking.api.model.entity.Booking;
-import se.almstudio.booking.api.model.entity.Hotel;
-import se.almstudio.booking.api.model.entity.RoomType;
+
+import se.almstudio.booking.api.model.entity.CityCountryRoomType;
 import se.almstudio.booking.api.repository.impl.DefaultRoomRepository;
 import se.almstudio.booking.api.service.BookingService;
 import se.almstudio.booking.api.util.ConnectionManager;
@@ -14,8 +13,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,111 +21,32 @@ public class DefaultBookingService implements BookingService {
   private static final Logger LOGGER = LoggerFactory.getLogger(DefaultRoomRepository.class);
 
   @Override
-  public List<Booking> findBooking(LocalDate checkInDate, LocalDate checkOutDate) {
-    LOGGER.info("Finding list of available hotels between checkIn and checkOut date");
-    Connection connection = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-    try {
-      connection = ConnectionManager.INSTANCE.getConnection();
-      String query = "SELECT * FROM booking WHERE ? < checkInDate or ? > checkOutDate";
-      ps = connection.prepareStatement(query);
-      ps.setObject(1, checkOutDate);
-      ps.setObject(2, checkInDate);
-      ps.execute();
-      rs = ps.getResultSet();
-      List<Booking> bookings = new ArrayList<>();
-      while (rs.next()) {
-        Booking booking = new Booking();
-        booking.setId(rs.getLong("ID"));
-        booking.setUserId(rs.getLong("userId"));
-        booking.setCheckInDate(rs.getObject("checkInDate", LocalDate.class));
-        booking.setCheckOutDate(rs.getObject("checkOutDate", LocalDate.class));
-        booking.setHotelId(rs.getLong("hotelId"));
-        booking.setRoomId(rs.getLong("roomId"));
-        booking.setRegistered(rs.getObject("registered", LocalDateTime.class));
-        booking.setUpdated(rs.getObject("Updated", LocalDateTime.class));
-        bookings.add(booking);
-      }
-      LOGGER.debug("Bookings were found");
-      return bookings;
-    } catch (SQLException e) {
-      LOGGER.warn("Failed to find bookings");
-      throw new RuntimeException(e);
-    } finally {
-      ConnectionUtils.closeQuietly(rs);
-      ConnectionUtils.closeQuietly(ps);
-      ConnectionUtils.closeQuietly(connection);
-    }
-  }
-
-  @Override
-  public List<Hotel> findHotelByCityAndCountry(String city, String country) {
+  public List<CityCountryRoomType> findHotelByCityAndCountry(String city, String country, String name) {
     LOGGER.info("Finding list of available hotels in the city and country");
     Connection connection = null;
     PreparedStatement ps = null;
     ResultSet rs = null;
     try {
       connection = ConnectionManager.INSTANCE.getConnection();
-      String query = "SELECT * FROM hotel WHERE city = ? AND country = ?";
+      String query = "SELECT * from hotel join roomtype ON hotel.id = roomtype.hotelid WHERE hotel.city= ? AND hotel.country = ? AND roomType.name = ?";
       ps = connection.prepareStatement(query);
       ps.setString(1, city);
       ps.setString(2, country);
+      ps.setString(3, name);
       ps.execute();
       rs = ps.getResultSet();
-      List<Hotel> hotels = new ArrayList<>();
+      List<CityCountryRoomType> hotels = new ArrayList<>();
       while (rs.next()) {
-        Hotel hotel = new Hotel();
-        hotel.setId(rs.getLong("ID"));
-        hotel.setName(rs.getString("name"));
-        hotel.setAddress(rs.getString("address"));
-        hotel.setCity(rs.getString("city"));
-        hotel.setCountry(rs.getString("country"));
-        hotel.setRegistered(rs.getObject("registered", LocalDateTime.class));
-        hotel.setUpdated(rs.getObject("Updated", LocalDateTime.class));
-        hotels.add(hotel);
+        CityCountryRoomType cityCountryRoomType = new CityCountryRoomType();
+        cityCountryRoomType.setCity(rs.getString("city"));
+        cityCountryRoomType.setCountry(rs.getString("country"));
+        cityCountryRoomType.setName(rs.getString("name"));
+        hotels.add(cityCountryRoomType);
       }
-      LOGGER.debug("hotel were found");
+      LOGGER.debug("{} hotel were found with city={}, country={} and roomType={}", hotels.size(), city, country, name);
       return hotels;
     } catch (SQLException e) {
       LOGGER.warn("Failed to find hotel");
-      throw new RuntimeException(e);
-    } finally {
-      ConnectionUtils.closeQuietly(rs);
-      ConnectionUtils.closeQuietly(ps);
-      ConnectionUtils.closeQuietly(connection);
-    }
-  }
-
-  @Override
-  public List<RoomType> findRoomType(int Capacity) {
-    LOGGER.info("Finding list of available hotels with the roomType");
-    Connection connection = null;
-    PreparedStatement ps = null;
-    ResultSet rs = null;
-    try {
-      connection = ConnectionManager.INSTANCE.getConnection();
-      String query = "SELECT * FROM roomType WHERE capacity = ?";
-      ps = connection.prepareStatement(query);
-      ps.setInt(1, Capacity);
-      ps.execute();
-      rs = ps.getResultSet();
-      List<RoomType> roomTypes = new ArrayList<>();
-      while (rs.next()) {
-        RoomType roomType = new RoomType();
-        roomType.setId(rs.getLong("ID"));
-        roomType.setHotelId(rs.getLong("hotelId"));
-        roomType.setName(rs.getString("name"));
-        roomType.setDescription(rs.getString("description"));
-        roomType.setCapacity(rs.getInt("capacity"));
-        roomType.setRegistered(rs.getObject("registered", LocalDateTime.class));
-        roomType.setUpdated(rs.getObject("Updated", LocalDateTime.class));
-        roomTypes.add(roomType);
-      }
-      LOGGER.debug("roomType were found");
-      return roomTypes;
-    } catch (SQLException e) {
-      LOGGER.warn("Failed to find roomType");
       throw new RuntimeException(e);
     } finally {
       ConnectionUtils.closeQuietly(rs);
